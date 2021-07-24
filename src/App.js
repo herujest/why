320dba4df314f7ef6ad4a835dc36b391e0341e8b
heruu.js <heruu.js@wehelpyou.id>
2021-07-24 23:59:28 +0700

    edit main screen

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./assets/icons/wehelpyou-logo.png";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h3>Wehelpyou</h3>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>This is unofficial mobile documentation</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Click here to skip
         </a>
       </header>
     </div>
