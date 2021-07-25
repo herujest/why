48f61c012aadfd68e884eaeb74d6af7945e6065c
heruu.js <heruu.js@wehelpyou.id>
2021-07-26 02:48:29 +0700

    Merge branch 'source' of github.com:slrwinter/slrwinter.github.io-why into source

--- src/components/pages/Home/home.jsx
+++ src/components/pages/Home/home.jsx
@@ -4,7 +4,7 @@ import { Button, Col, Jumbotron as Jumbo, Row } from 'react-bootstrap';
 export default class Home extends Component {
   render() {
     return (
-      <div>
+      <div id="home">
         <Jumbo fluid="true" className="jumbotron jumbotron-fluid ">
           <div className="jumbo-content">
             <Row>
@@ -29,6 +29,35 @@ export default class Home extends Component {
             </Row>
           </div>
         </Jumbo>
+        <div className="home-content">
+          <Row>
+            <Col sm="4">
+              <h4>
+                Our Libraries
+              </h4>
+              <p className="paragraf">
+                Libraries collectively include all the UI and visual components used across all Wehelpyou products. The designer can simply link any of the libraries to the Figma page he/she is working on, and instantly access all the components within the library directly from Sketch. The libraries are constantly updated to feature the latest UI components across all platforms, so as to provide a centralised repository which features all the latest design elements.
+              </p>
+            </Col>
+            <Col sm="4">
+              <h4>
+                The founding pillars
+              </h4>
+              <p className="paragraf">
+                Fundamental design rules to help you create amazing user experiences, including guidelines to use Colors pallete, typhography, motion and others UI view.
+              </p>
+            </Col>
+            <Col sm="4">
+              <h4>
+                API request
+              </h4>
+              <p className="paragraf">
+                Describe how we used Wehelpyou API by giving API request and see its responses. And we can provide what endpoint that currently in use for mobile client
+              </p>
+            </Col>
+          </Row>
+        </div>
+
       </div>
     );
   }
